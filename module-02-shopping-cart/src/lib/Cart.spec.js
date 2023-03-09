1e52e1398407647c398c70949602208ddcd67147
William Koller <williamkoller404@gmail.com>
2023-03-09 20:30:18 -0300

    test: :white_check_mark: should multiply quantity and price nd receive the total amount

--- module-02-shopping-cart/src/lib/Cart.spec.js
+++ module-02-shopping-cart/src/lib/Cart.spec.js
@@ -1,9 +1,26 @@
 import Cart from './Cart';
 
 describe('Cart', () => {
-  it('should return 0 when getTotal() is executed in a newly created instance', () => {
-    const cart = new Cart();
+  let cart;
 
+  beforeEach(() => {
+    cart = new Cart();
+  });
+  it('should return 0 when getTotal() is executed in a newly created instance', () => {
     expect(cart.getTotal()).toEqual(0);
+  });
+
+  it('should multiply quantity and price nd receive the total amount', () => {
+    const item = {
+      product: {
+        title: 'Adidas running shoed - men',
+        price: 35388, // 353.88 | R$ 383.88
+      },
+      quantity: 2, // 70776
+    };
+
+    cart.add(item);
+
+    expect(cart.getTotal()).toEqual(70776);
   });
 });
