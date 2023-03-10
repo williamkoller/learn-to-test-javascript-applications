91a1072c97ecb1ac0a9d319bd622f53c8b499cde
William Koller <williamkoller404@gmail.com>
2023-03-10 11:33:59 -0300

    test: :white_check_mark: move desc

--- module-02-shopping-cart/src/lib/Cart.spec.js
+++ module-02-shopping-cart/src/lib/Cart.spec.js
@@ -15,48 +15,51 @@ describe('Cart', () => {
   beforeEach(() => {
     cart = new Cart();
   });
-  it('should return 0 when getTotal() is executed in a newly created instance', () => {
-    expect(cart.getTotal()).toEqual(0);
-  });
 
-  it('should multiply quantity and price nd receive the total amount', () => {
-    const item = {
-      product,
-      quantity: 2, // 70776
-    };
-
-    cart.add(item);
-
-    expect(cart.getTotal()).toEqual(70776);
-  });
-
-  it('should ensure no more than on product exists at a time', () => {
-    cart.add({
-      product,
-      quantity: 2,
+  describe('getTotal()', () => {
+    it('should return 0 when getTotal() is executed in a newly created instance', () => {
+      expect(cart.getTotal()).toEqual(0);
     });
 
-    cart.add({
-      product,
-      quantity: 1,
+    it('should multiply quantity and price nd receive the total amount', () => {
+      const item = {
+        product,
+        quantity: 2, // 70776
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal()).toEqual(70776);
     });
 
-    expect(cart.getTotal()).toEqual(35388);
-  });
+    it('should ensure no more than on product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
 
-  it('should update total when a product gets included and then removed', () => {
-    cart.add({
-      product,
-      quantity: 2,
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
     });
 
-    cart.add({
-      product: product2,
-      quantity: 1,
+    it('should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(41872);
     });
-
-    cart.remove(product);
-
-    expect(cart.getTotal()).toEqual(41872);
   });
 });
