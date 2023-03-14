a15c35de5442ff63d17e84a3a3e9d9a0292c3b14
William Koller <williamkoller404@gmail.com>
2023-03-14 15:57:57 -0300

    feat: :sparkles: should apply percentage discount when certain quantity threshold is passed

--- module-02-shopping-cart/src/lib/Cart.js
+++ module-02-shopping-cart/src/lib/Cart.js
@@ -1,5 +1,19 @@
 import find from 'lodash/find';
 import remove from 'lodash/remove';
+import Dinero from 'dinero.js';
+
+const Money = Dinero;
+
+Money.defaultCurrency = 'BRL';
+Money.defaultPrecision = 2;
+
+const calculatePercentageDiscount = (amount, item) => {
+  if (item.condition?.percentage && item.quantity > item.condition.minimum) {
+    return amount.percentage(item.condition.percentage);
+  }
+
+  return Money({ amount: 0 });
+};
 
 export default class Cart {
   items = [];
@@ -18,12 +32,15 @@ export default class Cart {
 
   getTotal() {
     return this.items.reduce((acc, item) => {
-      return acc + item.quantity * item.product.price;
-    }, 0);
+      const amount = Money({ amount: item.quantity * item.product.price });
+      const discount = calculatePercentageDiscount(amount, item);
+
+      return acc.add(amount).subtract(discount);
+    }, Money({ amount: 0 }));
   }
 
   summary() {
-    const total = this.getTotal();
+    const total = this.getTotal().getAmount();
     const items = this.items;
 
     return {
